fa8c5f78380b8ee6af3c12a25d2e39f6a792c82b
Javier Moreno <email@javier-moreno.com>
2014-12-05 10:02:11 +0100

    Cambiado nombre rutina a la anterior.

--- www/infotelefonoplugin.js
+++ www/infotelefonoplugin.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 console.info('Registrando plugin InfoTelefono.');	
 
 var instancia = {
-	getInfo : function(successCallback,failureCallback) {
+	obtenerInfo  : function(successCallback,failureCallback) {
 	 exec(successCallback, failureCallback, 
 	      'infotelefonoplugin', 'ACCION_OBTENER_TELEFONO', []);
 		
